6f5b30a1bfbd82d64dee033a6a0c708c6f89bdbc
black <black@example.com>
2026-07-05 17:57:36 +0500

    Fix code: call greetWhite and greetBlack functions

--- console_black.py
+++ console_black.py
@@ -1,3 +1,10 @@
-print("hello everyone")
+def greetWhite():
+    print("hello white")
+
 def greetBlack():
     print("hello black")
+
+# Вызов функций после вывода «hello everyone»
+print("hello everyone")
+greetWhite()
+greetBlack()
